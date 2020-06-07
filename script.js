48a87b4027b87c04e6a611932e8f702053c663cb
Aman Deep Deep <aman220307@gmail.com>
2020-06-07 12:34:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("aman")
+amann="aman"
+
+
+function aman(){
+  var h1=document.createElement('h1');
+  text=document.createTextNode(amann)
+  h1.appendChild(text);
+  document.getElementById("a").appendChild(h1);
+}
